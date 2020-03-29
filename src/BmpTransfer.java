99882f0557b2164f111eccb37e025595e81ff803
aduyinuo <yinuod@andrew.cmu.edu>
2020-03-28 23:54:53 -0400

    Logger

--- src/BmpTransfer.java
+++ src/BmpTransfer.java
@@ -3,6 +3,10 @@
  * First Name: Yinuo
  * Last Name: Du
  */
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.opencv.core.*;
 import org.opencv.imgcodecs.*;
 
@@ -12,6 +16,7 @@ public class BmpTransfer {
     private boolean keepAlpha;
     private boolean anyDepth;
     private int readFlag;
+    private Logger logger;
 
     public void setKeepAlpha(boolean keepAlpha) {
         this.keepAlpha = keepAlpha;
@@ -31,7 +36,7 @@ public class BmpTransfer {
             this.readFlag |= Imgcodecs.IMREAD_COLOR;
         }
         if (this.anyDepth) this.readFlag |= Imgcodecs.IMREAD_ANYDEPTH;
-        System.out.println("Read flag: " + this.readFlag);
+        logger.log(Level.INFO, "Read flag: " + this.readFlag);
     }
 
     public int getReadFlag() {
@@ -51,31 +56,46 @@ public class BmpTransfer {
             this.readFlag |= Imgcodecs.IMREAD_COLOR;
         }
         if (this.anyDepth) this.readFlag |= Imgcodecs.IMREAD_ANYDEPTH;
+
+        this.logger = Logger.getLogger(BmpTransfer.class.getName()); 
     }
 
     public void convertPhoto(String inputFile, String outputFile) {
+        // Check input file path existence
+        File inDir = new File(inputFile);
+        if (!inDir.exists()) {
+            logger.log(Level.INFO, "BMP file doesn't exist!");
+            return;
+        }
+
         // Load negative image 
         Mat negMatrix = this.imageCodecs.imread(inputFile, this.readFlag);
         if (negMatrix == null) {
-            System.out.println("BMP read failure");
+            logger.log(Level.INFO, "BMP read failure!");
             return;
         }
-        // Meta-date of neg-image
-        System.out.println("Image Loaded, channels: " + negMatrix.channels() + ", dim:  " + negMatrix.dims() 
+
+        // Meta-data of neg-image
+        logger.log(Level.INFO, "Image Loaded, channels: " + negMatrix.channels() + ", dim:  " + negMatrix.dims() 
         + ", elemSize1: " + negMatrix.elemSize1() + ", depth: " + negMatrix.depth());
 
         Size imgSize = negMatrix.size();
         Mat orgMatrix = negMatrix.clone();
+
+        // calculate original image 
         int colorRange = (1 << (negMatrix.elemSize1() * 8)) - 1;
         for (int i = 0; i < imgSize.height; i++) {
             for (int j = 0; j < imgSize.width; j++) {
                 double[] data = negMatrix.get(i, j);
+                // Always read in BGR color space
                 data[0] = colorRange - data[0];
                 data[1] = colorRange - data[1];
                 data[2] = colorRange - data[2];
                 orgMatrix.put(i, j, data);
             }
         }
+
+        // write to target output path
         this.imageCodecs.imwrite(outputFile, orgMatrix);
     }
 }
